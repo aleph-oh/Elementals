56c0b59c11172805e403b379c7412604605c6b70
Jay Raymond Hilton <jay@vindaloo.com>
2018-06-18 19:26:07 -0400

    Fire elemental data in

---
+++ Elementals.py
@@ -0,0 +1,7 @@
+'''The first part of this program deals with inputting the properties of
+all elementals in for the usage of users.'''
+fire_hp = 1000#Fire elemental hp
+fire_mp = 500#Fire elemental maximum mana
+fire_atk = 1.2#Fire elemental attack multiplier
+fire_def = 0.9#Fire elemental defense multiplier
+fire_spd = 1#Fire elemental speed multiplier
