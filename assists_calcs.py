e8ad8311c31c678011ee7af445dc0503765e911d
Jay Raymond Hilton <jay@vindaloo.com>
2018-06-18 22:08:33 -0400

    All elementals known implemented

--- assists_calcs.py
+++ assists_calcs.py
@@ -19,15 +19,43 @@ earth = [base_hp, base_mp, 1, 1.3, 0.8, earth_adv, earth_dis] #Overall earth sta
 
 wind_adv = ['Smoke', 'Steam', 'Sand'] #Wind elemental offensive advantages
 wind_dis = ['Lightning', 'Ice', 'Crystal'] #Wind elemental offensive disadvantages
-wind = [base_hp, base_mp, 1, 0.9, 1.2, wind_adv, wind_dis]
+wind = [base_hp, base_mp, 1, 0.9, 1.2, wind_adv, wind_dis] #Overall wind stats
 
 lightning_adv = ['Wind', 'Water', 'Smoke'] #Lightning elemental offensive advantages
 lightning_dis = ['Plasma', 'Sand', 'Storm'] #Lightning elemental offensive disadvantages
-lightning = [base_hp, base_mp, 1, 0.8, 1.3, lightning_adv, lightning_dis]j
+lightning = [base_hp, base_mp, 1, 0.8, 1.3, lightning_adv, lightning_dis] #Overall lightning stats
 
 #Delta Elementals [hp, mp, atk, def, spd, adv, dis]
 
 magma_adv = ['Earth', 'Steam', 'Ice', 'Flora'] #Magma elemental offensive advantages
 magma_dis = ['Fire', 'Water', 'Thunder'] #Magma elemental defensive advantages
-magma = [850, 500, 1.4, 1, 0.7, magma_adv, magma_dis]
+magma = [850, 500, 1.4, 1, 0.7, magma_adv, magma_dis] #Overall magma stats
 
+smoke_adv = ['Lightning', 'Plasma', 'Flora'] #Smoke elemental offensive advantages
+smoke_dis = ['Wind', 'Steam', 'Ice'] #Smoke elemental offensive disadvantages
+smoke = [1100, 500, 0.8, 1.2, 1.1, smoke_adv, smoke_dis] #Overall smoke stats
+
+plasma_adv = ['Wind', 'Water', 'Storm'] #Plasma elemental offensive advantages
+plasma_dis = ['Earth', 'Magma', 'Sand'] #Plasma elemental offensive disadvantages
+plasma = [650, 650, 1.3, 0.6, 1.2, plasma_adv, plasma_dis] #Overall plasma stats
+
+steam_adv = ['Smoke', 'Magma', 'Ice'] #Steam elemental offensive advantages
+steam_dis = ['Wind', 'Water', 'Thunder', 'Storm'] #Steam elemental offensive disadvantages
+steam = [900, 550, 1, 1.2, 0.9] #Overall steam stats
+
+sand_adv = ['Fire', 'Lightning', 'Magma', 'Plasma'] #Sand elemental offensive advantages
+sand_dis = ['Wind', 'Earth', 'Crystal'] #Sand elemental offensive disadvantages
+sand = [1200, 550, 1, 1.4, 0.7, sand_adv, sand_dis] #Overall sand stats
+
+thunder_adv = ['Wind', 'Water', 'Crystal'] #Thunder elemental offensive advantages
+thunder_dis = ['Earth', 'Sand', 'Lightning'] #Thunder elemental offensive disadvantages
+thunder = [1000, 600, 1.1, 1.1, 0.9, thunder_adv, thunder_dis] #Overall thunder stats
+
+ice_adv = ['Earth', 'Sand', 'Flora', 'Storm'] #Ice elemental offensive advantages
+ice_dis = ['Fire', 'Magma', 'Plasma'] #Ice elemental offensive disadvantages
+ice = [1200, 550, 1.3, 0.7, 1.1, ice_adv, ice_dis] #Overall ice stats
+
+def damage_calc():
+    '''This program will solve for damage dealt from an attack with a given base power
+        between two elementals.'''
+    end
